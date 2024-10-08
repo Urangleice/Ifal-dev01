349a21cb48180a540859917b87db926d51aa5e0e
Gleice <ussp1@aluno.ifal.edu.br>
2024-10-08 10:59:38 -0300

    Update Untitled-1.py + hello world
    coloquei um hello world no fim do código

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,8 +5,9 @@ idade = input("Digite sua idade: ")
 
 print(f"Olá, {nome}! Você tem {idade} anos.")
 
+print ("Hello World!")
+
 if idade>=18:
   print("Você pode dirigir.")
 else:
   print("Você não pode dirigir.")
-
