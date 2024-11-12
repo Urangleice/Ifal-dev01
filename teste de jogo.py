a2a6fb7f03d8cfa78c454405c6d4fb77ecb51afa
Gleice <ussp1@aluno.ifal.edu.br>
2024-11-12 12:29:09 -0300

    Add files via upload

---
+++ teste de jogo.py
@@ -0,0 +1,13 @@
+print ("Bem-vindo, jogador!")
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print(f"Olá, {nome}! Você tem {idade} anos.")
+
+if idade > 18:
+  print("Você pode dirigir.")
+else:
+  print("Você não pode dirigir.")
+
+
